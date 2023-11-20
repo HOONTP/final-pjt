6f93e93dc1fcf715fba7660cdbacc87700ef1f87
HocheolHwang <inpofes7849@naver.com>
2023-11-21 08:57:03 +0900

    모닝
    ㅇㅇㅇ

--- dj/community/serializers.py
+++ dj/community/serializers.py
@@ -17,12 +17,10 @@ class CommentSerializer(serializers.ModelSerializer):
 
 class ArticleSerializer(serializers.ModelSerializer):
     comments = CommentSerializer(many=True, read_only=True) # 관계모델에서 related값과 동일하게
-
     class Meta:
         model = Article
         fields = '__all__'
         read_only_fields = ('user', 'like_users',)
-
 
 class ReplySerializer(serializers.ModelSerializer):
     user_nickname = serializers.CharField(source='user.nickname', read_only=True)
